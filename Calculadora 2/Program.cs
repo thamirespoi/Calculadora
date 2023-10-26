e31bd3fa0f6f57506a2305dd357a3d516e315b60
Aluno <Aluno@DESKTOP-J3E62P5>
2023-10-25 21:38:11 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                \Crie um programa que solicite 4 numeros e ao final mostre a média dos números.
+            */
+
+            // Define as variáveis
+            decimal numero_um;
+            decimal numero_dois;
+            decimal numero_tres;
+            decimal numero_quatro;
+
+            Console.WriteLine("MEDIA APP 3000");
+
+            Console.Write("Informe o primeiro numero: ");
+            numero_um = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o segundo numero: ");
+            numero_dois = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o terceiro numero: ");
+            numero_tres = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o quarto numero: ");
+            numero_quatro = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado_media = (numero_um + numero_dois + numero_tres 
+                + numero_quatro) / 4;
+
+            Console.WriteLine(
+                "A media dos numeros" + numero_um + "+" + numero_dois + "+" + numero_tres + 
+                "+" + numero_quatro + "é igual a: " + resultado_media + "."
+            );
+
+        }
+    }
+}
---
+++ CalculadoraPeso/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPeso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+                Crie um programa que solicite o nome, idade, sexo, peso e altura. Ao final 
+                utilize a fórmula "peso / altura * altura" para mostrar o IMC da pessoa
+
+            */
+
+            // Define as variáveis
+            string nome;
+            decimal idade;
+            string sexo;
+            decimal peso;
+            decimal altura;
+
+            Console.WriteLine("CALCULADORA DE PESO APP 3000");
+
+            Console.Write("Informe seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe sua idade: ");
+            idade = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe seu sexo: ");
+            sexo = Console.ReadLine();
+
+            Console.Write("Informe sua peso: ");
+            peso = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe sua altura: ");
+            altura = Convert.ToDecimal(Console.ReadLine());
+
+            decimal imc = peso / (altura * altura);
+
+            Console.WriteLine(
+                nome + ", sua idade é " + idade + ", seu genêro é " + sexo + ", seu peso é " 
+                + peso + " e sua altura é " + altura + ", fazendo sua média de IMC ser "
+                + imc + "."
+            );
+        }
+    }
+}
